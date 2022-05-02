6f41cd1c46c268bcc5db76f55a2b4bd34a8ee4f8
Guilherme Lameiras Fernandez <guil.fernandez1@hotmail.com>
2022-05-01 22:23:19 -0300

    created some functions to help predictions

--- app.py
+++ app.py
@@ -1,19 +1,14 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
+from api_routes import bp1
+from pycaret.regression import *
 
 app = Flask(__name__)
 
-cols = ['age', 'sex', 'cp', 'trestbps', 'chol', 'fbs', 'restecg', 'thalach', 'exang', 'oldpeak', 'slope', 'ca', 'thal', 'target']
+app.register_blueprint(bp1)
 
 @app.route("/")
 def home():
     return render_template("home.html")
-
-@app.route("/predict", methods=['POST'])
-def predict():
-    formValues = [x for x in request.form.values()]
-    print(formValues)
-    return render_template('home.html', pred = 'Hello World')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
