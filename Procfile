0b1702a25ed927dee684f168d5909620498ca7ac
Guilherme Lameiras Fernandez <guil.fernandez1@hotmail.com>
2022-05-04 19:12:03 -0300

    added comentary in code

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
+# Arquivo responsável para o Deploy da aplicação no Heroku.
 web: gunicorn app:app
--- static/js/home.js
+++ static/js/home.js
@@ -1,8 +1,10 @@
 $(document).ready(function (e) {
+    // Aguardando ocorrer o submit no id: runModel
     $('#runModel').submit(function (event) {
-
+        // Assim que ocorrer o submit, limpar a tag com id: hfResult
         $('#hfResult').empty();
 
+        // Coleta dos valores do formulário.
         var age = $('#age').val();
         var sex = $('#sex').val();
         var cp = $('#cp').val();
@@ -17,6 +19,8 @@ $(document).ready(function (e) {
         var ca = $('#ca').val();
         var thal = $('#thal').val();
 
+
+        // Inserção dos valores em um objeto javascript
         var inputData = {
             'age': age,
             'sex': sex,
@@ -33,19 +37,20 @@ $(document).ready(function (e) {
             'thal': thal
         };
 
+        // Requisição assincrona para o backend
         $.ajax({
-            url: this.action,
-            type: this.method,
+            url: this.action,  // /main/api/run_model
+            type: this.method, // POST
             data: inputData,
         })
-        .done(function (response) {
+        .done(function (response) { // Assim que feita a requisição assincrona e der sucesso, será executado o comando abaixo
             if(response.pred == 0) {
-                $('#hfResult').append(`<p>The Pacient does not have a Heart Disease</p>`)
+                $('#hfResult').append(`<p>The Pacient does not have a Heart Disease</p>`) // Retorno para o frontend se a "predict" for 0
             } else {
-                $('#hfResult').append(`<p>The Pacient has Heart Disease</p>`)
+                $('#hfResult').append(`<p>The Pacient has Heart Disease</p>`) // Retorno para o frontend se a "predict" for 1
             }
         });
-
+        // Cancela evento default de refresh de página
         event.preventDefault();
     });
 });
